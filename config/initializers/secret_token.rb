48588491b822ec2b77d85fb8944fa50a590c4464
Michael <michael.matrosov@gmail.com>
2016-08-29 20:19:12 +0300

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LightCloud::Application.config.secret_key_base = '12cde51c8acf46813836e974ba5b5372596cab3786fe719d27e831ccc9cdb2631e68f95595a44d85d78b0f8ad6e10d98e44f6eb62e770371d83fbe69c43f2265'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LightCloud::Application.config.session_store :cookie_store, key: '_light_cloud_session'
